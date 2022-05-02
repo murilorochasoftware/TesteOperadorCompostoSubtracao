e41c32cced0102773b8fab0460ffee01be1199c7
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 19:56:59 -0300

    Teste do uso do operador de atribuição de maneira composta com o operador de subtração em Java.

---
+++ src/TesteOperadorCompostoSubtracao.java
@@ -0,0 +1,9 @@
+public class TesteOperadorCompostoSubtracao {
+
+    public static void main(String[] args) {
+        byte quantidade = 20;
+        quantidade -= 5;
+
+        System.out.printf("Quantidade: %d", quantidade);
+    }
+}
